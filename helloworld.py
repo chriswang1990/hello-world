5699d23a7791d30309d52c6b988118560e2ae260
Minquan Wang <1990chriswang1990.gmail.com>
2016-11-23 01:30:11 -0800

    Create a form and validation for birthday input

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,57 @@
 import webapp2
 
 form = """
-<form action="http://www.google.com/search">
-  <input name="q">
-  <input type="submit">
+<form method="post"> 
+    What is your birthday?
+    <br>
+
+    <label> Month
+        <input type="text" name="month">
+    </label> 
+
+    <label> Day
+        <input type="text" name="day">
+    </label> 
+
+    <label> Year
+        <input type="text" name="year">
+    </label> 
+
+    <br>
+    <br>
+    <input type="submit">
 </form>
 """
 
 class MainPage (webapp2.RequestHandler) :
 
-  def get(self):
+    def get(self):
     
-    #self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write(form)
+        #self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write(form)
 
-app = webapp2.WSGIApplication([
-    ('/', MainPage),], debug = True)
+    def post(self):
+
+        user_month = valid_month(self.request.get("month"))
+        user_day = valid_month(self.request.get("day"))
+        user_year = valid_year(self.request.get("year"))
+
+        if not (user_month and user_day and user_year):
+            self.response.out.write(form)
+        else:
+            self.response.out.write("Thanks! That;s a totally valid day!")
+    
+    def valid_month(month):
+
+        months = ['January', 'February', 'March', 'April', 'May', 'June',
+            'July', 'August', 'September', 'October', 'November', 'December']
+        month_abbvs = dict((m[:3].lower(), m) for m in months
+        if month:
+            month = month[:3].lower()
+            return month_abbvs.get(month) 
+
+    def valid_day(day):
+        if day and day.isdigit():
+                    
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
