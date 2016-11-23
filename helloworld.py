4b222b23657b672a8b56f0d6a34409c0b4ceb14e
chriswang1990 <1990chriswang1990@gmail.com>
2016-11-23 12:28:04 -0800

    Refactor helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,30 +4,26 @@ form = """
 <form method="post"> 
     What is your birthday?
     <br>
-
     <label> Month
         <input type="text" name="month">
     </label> 
-
     <label> Day
         <input type="text" name="day">
     </label> 
-
     <label> Year
         <input type="text" name="year">
     </label> 
-
     <br>
     <br>
     <input type="submit">
 </form>
 """
 
-class MainPage (webapp2.RequestHandler) :
 
+class MainPage(webapp2.RequestHandler):
     def get(self):
-    
-        #self.response.headers['Content-Type'] = 'text/plain'
+
+        # self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(form)
 
     def post(self):
@@ -40,18 +36,19 @@ class MainPage (webapp2.RequestHandler) :
             self.response.out.write(form)
         else:
             self.response.out.write("Thanks! That;s a totally valid day!")
-                       
-app = webapp2.WSGIApplication([('/', MainPage)], debug = True)
+
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
 
 
 def valid_month(month):
-
     months = ['January', 'February', 'March', 'April', 'May', 'June',
-        'July', 'August', 'September', 'October', 'November', 'December']
-    month_abbvs = dict((m[:3].lower(), m) for m in months
+              'July', 'August', 'September', 'October', 'November', 'December']
+    month_abbvs = dict((m[:3].lower(), m) for m in months)
     if month:
         month = month[:3].lower()
-        return month_abbvs.get(month) 
+        return month_abbvs.get(month)
+
 
 def valid_day(day):
     if day and day.isdigit():
@@ -65,5 +62,3 @@ def valid_year(year):
         year = int(year)
         if year in range(1901, 2020):
             return year
-
-
