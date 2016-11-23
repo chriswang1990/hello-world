0c1deeee0333121d72157274ebd571d74711e14c
Minquan Wang <1990chriswang1990@gmail.com>
2016-11-23 15:55:17 -0800

    Add hint with string substitution to handle input

--- helloworld.py
+++ helloworld.py
@@ -13,6 +13,7 @@ form = """
     <label> Year
         <input type="text" name="year">
     </label> 
+    <div style="color: red">%(error)s</div>
     <br>
     <br>
     <input type="submit">
@@ -21,47 +22,48 @@ form = """
 
 
 class MainPage(webapp2.RequestHandler):
+    def write_form(self, error = ""):
+        self.response.out.write(form % {"error": error})
+
     def get(self):
 
         # self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
+        self.write_form()
 
     def post(self):
 
-        user_month = valid_month(self.request.get("month"))
-        user_day = valid_day(self.request.get("day"))
-        user_year = valid_year(self.request.get("year"))
+        user_month = self.valid_month(self.request.get("month"))
+        user_day = self.valid_day(self.request.get("day"))
+        user_year = self.valid_year(self.request.get("year"))
 
         if not (user_month and user_day and user_year):
-            self.response.out.write(user_month)
-            self.response.out.write(user_day)
-            self.response.out.write(user_year)
-            self.response.out.write(form)
+            self.write_form("The date input is not valid!")
         else:
             self.response.out.write("Thanks! That's a totally valid day!")
 
+    def valid_month(self, month):
+        months = ['January', 'February', 'March', 'April', 'May', 'June',
+                  'July', 'August', 'September', 'October', 'November', 'December']
+        month_abbvs = dict((m[:3].lower(), m) for m in months)
+        if month:
+            month = month[:3].lower()
+            return month_abbvs.get(month)
+
+
+    def valid_day(self, day):
+        if day and day.isdigit():
+            day = int(day)
+            if day in range(1, 32):
+                return day
+
+
+    def valid_year(self, year):
+        if year and year.isdigit():
+            year = int(year)
+            if year in range(1901, 2020):
+                return year
+            
 
 app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
 
 
-def valid_month(month):
-    months = ['January', 'February', 'March', 'April', 'May', 'June',
-              'July', 'August', 'September', 'October', 'November', 'December']
-    month_abbvs = dict((m[:3].lower(), m) for m in months)
-    if month:
-        month = month[:3].lower()
-        return month_abbvs.get(month)
-
-
-def valid_day(day):
-    if day and day.isdigit():
-        day = int(day)
-        if day in range(1, 32):
-            return day
-
-
-def valid_year(year):
-    if year and year.isdigit():
-        year = int(year)
-        if year in range(1901, 2020):
-            return year
