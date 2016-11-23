7f4d9737f3cbd97efb7a56f4b4c8ef2877771b8f
chriswang1990 <1990chriswang1990@gmail.com>
2016-11-23 12:52:34 -0800

    Test and debug

--- helloworld.py
+++ helloworld.py
@@ -32,6 +32,10 @@ class MainPage(webapp2.RequestHandler):
         user_day = valid_month(self.request.get("day"))
         user_year = valid_year(self.request.get("year"))
 
+        print(user_month)
+        print(user_day)
+        print(user_year)
+
         if not (user_month and user_day and user_year):
             self.response.out.write(form)
         else:
